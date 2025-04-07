38006b9ca1790ed6bc3b3e1c8fd6a1b0e26fa59c
cjdeshon <cayden.deshon151@gmail.com>
2025-04-07 17:16:00 +0100

    Added interactive section for nav links

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    // Handle nav clicks
+    $('.nav-link').click(function(e) {
+      e.preventDefault();
+      const target = $(this).data('target');
+  
+      $('.content-section').hide(); // Hide all
+      $('#' + target).fadeIn(200);  // Show selected section
+    });
+  
+    // Optional: Add hover description back for projects
     $('.project').each(function() {
       const info = $(this).data('info');
       $(this).append(`<div class="project-description">${info}</div>`);
